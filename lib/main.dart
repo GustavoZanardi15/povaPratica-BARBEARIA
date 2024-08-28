e934621835263ac8d29383c7043cc875f7a842f3
GustavoZanardi15 <ulianzanardig@gmail.com>
2024-08-28 16:13:12 -0300

    Atualizações no main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,14 @@ class PageForm extends StatefulWidget {
 
 class _PageFormState extends State<PageForm> {
   final _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autovalidate = AutovalidateMode.disabled; 
   var _user;
+  var _senha;
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      autovalidateMode: _autovalidate, 
       key: _formKey,
       child: Column(
         children: <Widget>[
@@ -33,8 +36,30 @@ class _PageFormState extends State<PageForm> {
             onSaved: (value) {
               _user = value;
             },
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Por favor, digite seu nome de usuário';
+              }
+              return null;
+            },
           ),
           SizedBox(height: 20),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Senha',
+            ),
+            onSaved: (value) {
+              _senha = value;
+            },
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Digite sua senha';
+              }
+              return null;
+            },
+          ),
           TextButton(
             style: TextButton.styleFrom(
               backgroundColor: Colors.blue,
@@ -49,8 +74,11 @@ class _PageFormState extends State<PageForm> {
             onPressed: () {
               if (_formKey.currentState!.validate()) { 
                 _formKey.currentState!.save(); 
-
                 print(_user); 
+              } else {
+                setState(() {
+                  _autovalidate = AutovalidateMode.always; 
+                });
               }
             },
           ),
